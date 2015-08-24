1938675ed03f4ce15c2f3ea8f286d09032a02625
Stephen David <stephen.david.06@gmail.com>
2015-08-25 10:37:03 +1200

    Created an external JS file and used JS to create dropdown

--- www/js/count-rooms.js
+++ www/js/count-rooms.js
@@ -1,22 +1,23 @@
-<script type="text/javascript">
-	$("#checkbox-kitchen").click(function() {
-		if ($(this).is(':checked')) {
-			var htmlStr =
-				'<h1> HELLO </h1>';
-				
-			$.fancybox(htmlStr, {
-				'width': 950,
-				'height': 1100,
-				'autoScale': false,
-				'transitionIn': 'none',
-				'transitionOut': 'none',
-				'hideOnContentClick': false
+$(document).on("pagecreate", "#main", function() {
+	var dropDown = document.getElementById("get-amt-kitchen")
+	for (i=0; i<=20; i++) {
+		var listItem = document.createElement("option");
+		listItem.text = i;
+		listItem.value = i;
+		dropDown.appendChild(listItem);
+	}
+	$("#checkbox-kitchen").on("change", function() {
+		var val = $(this).val();
+		$("#popup-header-kitchen").html(val);
+		if(document.getElementById('checkbox-kitchen').checked) {
+			$("#popup-kitchen").popup("open");
+			
+			$("#set-amt-kitchen").on("click", function() {
+				var drp = document.getElementById("get-amt-kitchen");
+				var value = drp.options[ drp.options.selectedIndex ].value;
+				$("#amt-kitchen").html(value);
+				$("#popup-kitchen").popup("close");
 			});
 		}
 	});
-	
-
-	function count-kitchen(){
-    
-	}
-</script>
+});
